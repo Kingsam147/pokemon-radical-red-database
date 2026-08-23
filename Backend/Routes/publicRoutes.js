74180d7bcc4daa5848e4e21b447ecdbf427596a7
Samuel Darius <Samueldarius1470@gmail.com>
2026-08-22 21:42:36 -0400

    feat: expose GET /public/guest-starter-pikachu

--- Backend/Routes/publicRoutes.js
+++ Backend/Routes/publicRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getEnemyPreview } = require('../enemy-preview/publicControllers');
+const { getGuestStarterPikachu } = require('../guest-starter/publicControllers');
 
 router.route('/enemy-preview').get(getEnemyPreview);
+router.route('/guest-starter-pikachu').get(getGuestStarterPikachu);
 
 module.exports = router;
